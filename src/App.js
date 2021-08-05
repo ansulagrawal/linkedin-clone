f2a0475d87f66e10ceb4cc260cc9da52cfc454c5
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-08-05 16:11:03 +0530

    Created Login Page
    Login Page Creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      lets build linkedin clone
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
