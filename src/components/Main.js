3c1de7a3afbad065f96243f3e338d88927b9bbb8
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-08-06 00:13:32 +0530

    Home Page commit-3
    Added a social card that contains one image and a description. i.e  Article.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const Main = (props) => {
   return (
     <Container>
-       <ShareBox>
-                    {/* <div>
+      <ShareBox>
+        {/* <div>
                         { props.user && props.user.photoURL ?
                             (<img src={ props.user.photoURL} />)
                             :
@@ -17,34 +17,86 @@ const Main = (props) => {
                             Start a Post    
                         </button> 
                     </div> */}
-                    <div>
-                      <img src="/images/user.svg" className="post-icon" alt="" />
-                      <button>Start a post</button>
-                    </div>
-                    <div>
-                        <button>
-                            <img src="/images/photo-icon.svg" className="post-icon" alt="" />
-                            <span>Photo</span>
-                        </button>
+        <div>
+          <img src="/images/user.svg" className="post-icon" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" className="post-icon" alt="" />
+            <span>Photo</span>
+          </button>
 
-                        <button>
-                            <img src="/images/video-icon.svg" className="post-icon" alt="" />
-                            <span>Video</span>
-                        </button>
+          <button>
+            <img src="/images/video-icon.svg" className="post-icon" alt="" />
+            <span>Video</span>
+          </button>
 
-                        <button>
-                            <img src="/images/event-icon.svg" className="post-icon" alt="" />
-                            <span>Event</span>
-                        </button>
+          <button>
+            <img src="/images/event-icon.svg" className="post-icon" alt="" />
+            <span>Event</span>
+          </button>
 
-                        <button>
-                            <img src="/images/article-icon.svg" className="post-icon" alt="" />
-                            <span>Write article</span>
-                        </button>
-                    </div>
-                </ShareBox>
-
-  </Container>
+          <button>
+            <img src="/images/article-icon.svg" className="post-icon" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipsis.svg" alt="" />
+            </button>
+          </SharedActor>
+          <Description>Description</Description>
+          <SharedImg>
+            <a>
+              <img src="/images/shared-img.png" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img src="/images/liked-icon.svg" alt="" />
+                <img src="/images/clap-icon.svg" alt="" />
+                <span>75</span>
+              </button>
+            </li>
+            <li>
+              <a>2 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+            <button>
+              <img src="/images/like-icon.svg" alt="" />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src="/images/comment-icon.svg" alt="" />
+              <span>Comments</span>
+            </button>
+            <button>
+              <img src="/images/shared-comment.svg" alt="" />
+              <span>Comments</span>
+            </button>
+            <button>
+              <img src="/images/send-icon.svg" alt="" />
+              <span>send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </div>
+    </Container>
   );
 };
 
@@ -88,7 +140,7 @@ const ShareBox = styled(CommonCard)`
       display: flex;
       align-items: center;
       padding: 8px 16px 0px 16px;
-      color:blue;
+      color: blue;
 
       img {
         width: 48px;
@@ -105,7 +157,6 @@ const ShareBox = styled(CommonCard)`
         background-color: white;
         text-align: left;
       }
-
     }
 
     &:nth-child(2) {
@@ -121,9 +172,133 @@ const ShareBox = styled(CommonCard)`
           width: 25px;
           height: 25px;
         }
-        span{
-          color:#70b5f9;
+        span {
+          color: #70b5f9;
         }
+      }
+    }
+  }
+`;
+
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+
+      span {
+        text-align: left;
+
+        &::first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &::nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+
+  button{
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    color: #0a66c2;
+
+    @media (min-width:768px){
+      span{
+        margin-left: 8px;
       }
     }
   }
