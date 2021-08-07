a73c4997df9fbf56ac7318370d2fe3d0b353c3d1
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-08-07 20:00:24 +0530

    Added Post modal

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAfAbVYDbaTV4Fb-gWofwI943wlJhM0m1k",
+    authDomain: "linkedin-clone-c2de9.firebaseapp.com",
+    projectId: "linkedin-clone-c2de9",
+    storageBucket: "linkedin-clone-c2de9.appspot.com",
+    messagingSenderId: "989665980631",
+    appId: "1:989665980631:web:cfb90765449a64659f8092"
+  };
+  // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const storage = firebase.storage();
+
+  export { auth, provider, storage};
+  export default db;
